a4182bd01444572129004170f93609890deaf807
Amardeep Mann <amar2slick@gmail.com>
2023-03-24 15:50:28 -0700

    Peer review in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 // 3-24-23  
 // React Weather App  
 // We created a weather app in React based on our original Figma design that accesses the OpenWeatherAPI for its weather data  
-// Peer Reviewed by Reed Goodwin: 
+// Peer Reviewed by Reed Goodwin: A good looking and well put together weather app. Lacking a functioning favorites but the icons are there and with a little more work could easily be applied.  Cohesive and responsive!
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
